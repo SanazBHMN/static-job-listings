4dc43ec0be678a99e961c6802f2a545afcca967b
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-08-23 16:20:42 +0330

    Import assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        leagueSpartan: ["League Spartan", "sans-serif"],
+      },
+      colors: {
+        primary: "hsl(180, 29%, 50%)",
+        backgroud: "hsl(180, 52%, 96%)",
+        filterTablets: "hsl(180, 31%, 95%)",
+        text: "hsl(180, 8%, 52%)",
+        darkText: "hsl(180, 14%, 20%)",
+      },
+    },
   },
   plugins: [],
 };
