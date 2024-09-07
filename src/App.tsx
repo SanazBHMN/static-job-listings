1a9148fe43e983b5cd8113ccd581c357de0b0bbb
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-09-07 15:44:46 +0330

    Display jobs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import JobCard from "./components/JobCard";
 
 import data from "./data.json";
 
 function App() {
+  const [jobs, setJobs] = useState(data);
+
   return (
-    <>
+    <div className="bg-backgroud">
       <header className="bg-primary h-36 bg-headerMobile bg-repeat-x sm:bg-headerDesktop">
         <div></div>
       </header>
-      <main>
-        <JobCard />
+      <main className="sm:mt-20 h-full">
+        {jobs.map((job) => (
+          <JobCard job={job} />
+        ))}
       </main>
-    </>
+    </div>
   );
 }
 
--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,46 +1,43 @@
-import data from "../data.json";
-
-function JobCard() {
-  const item = data[0];
+function JobCard({ job }) {
   return (
-    <div className="border m-auto shadow-xl shadow-filterTablets">
-      <section className="p-6 relative">
-        <div className="w-14 h-14 absolute top-[-30px]">
-          <img src={item.logo} alt="logo" />
+    <div className="w-11/12 m-auto bg-white rounded-md my-12 shadow-2xl shadow-filterTablets sm:w-10/12 sm:my-6">
+      <section className="p-6 relative md:static md:flex md:items-center gap-5">
+        <div className="w-14 h-14 absolute top-[-30px] md:static md:w-16 md:h-16">
+          <img src={job.logo} alt="logo" />
         </div>
-        <div className="mt-4">
+        <div className="mt-4 md:w-full">
           <div className="flex justify-start items-center gap-6">
-            <h3 className="text-primary font-bold">{item.company}</h3>
+            <h3 className="text-primary font-bold">{job.company}</h3>
             <div className="flex gap-4">
-              {item.new && (
+              {job.new && (
                 <p className="w-full h-full bg-primary px-3 pt-1 text-white rounded-full">
                   NEW!
                 </p>
               )}
-              {item.featured && (
+              {job.featured && (
                 <p className="w-full h-full bg-darkText px-3 pt-1 text-white rounded-full">
                   FEATURED
                 </p>
               )}
             </div>
           </div>
-          <div className="flex flex-col items-start sm:flex-row sm:justify-between sm:items-center text-text">
-            <div className="w-full my-3 pb-3 border-b-2 sm:border-none">
-              <h3 className="my-3 font-semibold text-darkText">
-                {item.position}
+          <div className="flex flex-col items-start sm:flex-row sm:justify-between text-text">
+            <div className="w-full my-3 pb-3 border-b-2 sm:border-none md:my-1">
+              <h3 className="my-3 font-semibold text-darkText md:my-1">
+                {job.position}
               </h3>
-              <span>{item.postedAt} &#x2022;</span>
-              <span className="ml-2">{item.contract} &#x2022;</span>
-              <span className="ml-2">{item.location}</span>
+              <span>{job.postedAt} &#x2022;</span>
+              <span className="ml-2">{job.contract} &#x2022;</span>
+              <span className="ml-2">{job.location}</span>
             </div>
             <ul className="flex flex-wrap gap-4 mt-1 sm:flex-nowrap">
               <li className="bg-filterTablets px-3 pt-1 text-primary rounded-sm">
-                {item.role}
+                {job.role}
               </li>
               <li className="bg-filterTablets px-3 pt-1 text-primary rounded-sm">
-                {item.level}
+                {job.level}
               </li>
-              {item.languages.map((language) => (
+              {job.languages.map((language) => (
                 <li
                   key={language}
                   className="bg-filterTablets px-3 pt-1 text-primary rounded-sm"
@@ -48,7 +45,7 @@ function JobCard() {
                   {language}
                 </li>
               ))}
-              {item.tools.map((tool) => (
+              {job.tools.map((tool) => (
                 <li
                   key={tool}
                   className="bg-filterTablets px-3 pt-1 text-primary rounded-sm"
